63b5098d517a8fafe1ecc7c054d5b49b798b7ecd
Stefanie Onsori-Wechtitsch <74794671+stefOns@users.noreply.github.com>
2022-04-21 13:34:11 +0200

    Delete convertScannet-to-coco_script.bat
    removed

--- convertScannet-to-coco_script.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set scenename=%1
-echo %scenename%
-python scannet-to-coco.py --scannet_scene_dir \\avm-media\Storage\Datasets\ScanNet\solov2\train\%scenename% --export_color_images --coco_annotation_dir \\avm-media\Storage\Datasets\ScanNet\solov2\annotations --coco_annotation_file train-tol5-%scenename%.json --tolerance 5 
-REM python scannet-to-coco.py --scannet_scene_dir \\avm-media\Storage\Datasets\ScanNet\solov2\val\%scenename% --export_color_images --coco_annotation_dir \\avm-media\Storage\Datasets\ScanNet\solov2\annotations --coco_annotation_file val-tol5-%scenename%.json --tolerance 5 
- 
